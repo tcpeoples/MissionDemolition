7b5abf2ad54f88af76678c950f771cf14e23510f
tcpeoples <81200355+tcpeoples@users.noreply.github.com>
2022-02-15 14:40:40 -0600

    Finished making clouds
    game instantiates clouds upon start. clouds move

--- MissionDemolitionUnity/Assets/Scripts/Cloud.cs
+++ MissionDemolitionUnity/Assets/Scripts/Cloud.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Cloud : MonoBehaviour
 {
-
     [Header("Set in Inspector")]
 
     public GameObject cloudSphere;
@@ -23,27 +22,49 @@ public class Cloud : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        spheres = new List<GameObject>();
-        int num = Random.Range(numSpheresMin, numSpheresMax);
+       spheres = new List<GameObject>();
 
-        for (int i = 0, i<num, i++)
-        {
-            GameObject sp = Instantiate<GameObject>(cloudSphere);
-            spheres.Add(sp);
-            
-            Transform spTrans = sp.transform;
-            spTrans.SetParent(this.transform);
+       int num = Random.Range(numSpheresMin, numSpheresMax);
+       for (int i=0; i<num; i++)
+       {
+           GameObject sp = Instantiate<GameObject>(cloudSphere);
+           spheres.Add(sp);
+           Transform spTrans = sp.transform;
+           spTrans.SetParent(this.transform);
 
-            Vector3 offset = Random.insideUnitSphere;
-            offset.x *= sphereOffsetScale.x;
-            offset.y *= sphereOffsetScale.y;
-            offset.z *= sphereOffsetScale.z;
-        }
+           Vector3 offset = Random.insideUnitSphere;
+           offset.x *= sphereOffsetScale.x;
+           offset.y *= sphereOffsetScale.y;
+           offset.z *= sphereOffsetScale.z;
+            spTrans.localPosition = offset;
+
+            Vector3 scale = Vector3.one;
+            scale.x = Random.Range(sphereScaleRangeX.x, sphereScaleRangeX.y);
+            scale.y = Random.Range(sphereScaleRangeY.x, sphereScaleRangeY.y);
+            scale.z = Random.Range(sphereScaleRangeZ.x, sphereScaleRangeZ.y);
+
+            scale.y *= 1 - (Mathf.Abs(offset.x) / sphereOffsetScale.x);
+            scale.y = Mathf.Max(scale.y, scaleYMin);
+
+            spTrans.localScale = scale;
+       }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Restart();
+        //}
+    }
+
+    void Restart()
+    {
+        foreach(GameObject sp in spheres)
+        {
+            Destroy(sp);
+        }
+        Start();
     }
 }
